ba3683ca84ab56ffa0726d01802768dbc8b50165
megan <megan.lea.d@gmail.com>
2022-01-23 18:26:39 -0500

    added spot for an image on challenge ui

--- src/pages/Challenge/index.tsx
+++ src/pages/Challenge/index.tsx
@@ -8,6 +8,9 @@ import {
   AccordionButton,
   AccordionIcon,
   AccordionPanel,
+  Flex,
+  Container,
+  Divider,
 } from '@chakra-ui/react';
 import './challenge.css';
 import img1 from '../../assets/images/ultra-sound-money/1.jpeg'
@@ -35,52 +38,79 @@ const imgs = [
 
 const Challenge = () => {
   return (
-    <Box m="0 10%">
-      <Box height={30} />
-      <VStack align="start" spacing="0px">
-        <Heading
-          fontSize="4xl"
-          fontWeight="bold"
-          fontFamily="'Work Sans', sans-serif;"
-        >
-          Create an original Ultra Sound Money meme.
-        </Heading>
-        <Text>
-          By: kylekaplan.eth
-        </Text>
-        {/* <Text>
-          🏆 &nbsp; Ξ0.01 ETH
-        </Text> */}
-      </VStack>
+    <Box m="30px 30px">
+      <Flex>
+        <Box size="30vw" bg="black" p={4} alignItems="start" justifyContent="start">
+          <Text><b>Title:</b> Create an original Ultra Sound Money meme.</Text>
+          <Text><b>Posted by:</b> kylekaplan.eth</Text>
+          <Text><b>Start Date:</b> January 7th, 2022</Text>
+          <Text><b>End Date:</b> January 14th, 2022</Text>
+          <Text><b>Prize amount:</b> 0.01 ETH</Text>
+        </Box>
+        <Box p={5}>
+          <VStack align="start" spacing={3}>
+          <Text>
+            kylekaplan.eth
+          </Text>
+          <Heading
+            fontSize="4xl"
+            fontWeight="bold"
+            fontFamily="'Work Sans', sans-serif;"
+          >
+            Create an original Ultra Sound Money meme.
+          </Heading>
+          {/* <Text>
+            By: kylekaplan.eth
+          </Text> */}
+          {/* <Text>
+            🏆 &nbsp; Ξ0.01 ETH
+          </Text> */}
+        </VStack>
+        <Box mt={10} border="1px" borderRadius={8}>
+            <Box p="10px 5px">
+              <Text>
+                Challenge ends January 14, 2022 at 6:15am EST
+              </Text>
+              <TimeLeft date={Date.now() + 100000000} />
+            </Box>
+            <Divider />
+            <Box p="10px 5px">
+              <Text>
+                Prize: 0.01 ETH
+              </Text>
+            </Box>
+          </Box>
+        <Accordion mt={14} mb={10}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex='1' textAlign='left'>
+                  DETAILS
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+            <VStack fontSize="lg" spacing="5px" align="start">
+              <Text>
+                🏆 &nbsp; Ξ0.01 ETH
+              </Text>
+              <Text>
+                📅 &nbsp;Jan. 7th - 14th
+              </Text>
+              <Text>
+                ⏳ &nbsp;
+                <TimeLeft date={Date.now() + 100000000} />
+              </Text>
+            </VStack>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+        </Box>
+      </Flex>
       <Text marginTop="30px" fontSize="2xl">
         The meme must be in favor of the Ultra Sound Money idea and ideally funny and/or educational.
       </Text>
-      <Accordion mt={14} mb={10}>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex='1' textAlign='left'>
-                DETAILS
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-          <VStack fontSize="lg" spacing="5px" align="start">
-            <Text>
-              🏆 &nbsp; Ξ0.01 ETH
-            </Text>
-            <Text>
-              📅 &nbsp;Jan. 7th - 14th
-            </Text>
-            <Text>
-              ⏳ &nbsp;
-              <TimeLeft date={Date.now() + 100000000} />
-            </Text>
-          </VStack>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
       <Text fontSize="2xl">
         <b>Good Examples:</b>
       </Text>
