67c12f56026780efb7c6e1fff366ee574b0fd7b7
kyle kaplan <kylekaplan50@gmail.com>
2022-02-14 15:34:36 -0700

    added ipfs uploading

--- src/pages/Challenge/Uploader/index.tsx
+++ src/pages/Challenge/Uploader/index.tsx
@@ -1,3 +1,4 @@
+import { SetStateAction, useState } from 'react';
 import {
   VStack,
   Text,
@@ -5,8 +6,11 @@ import {
   useStyleConfig,
   Flex,
 } from '@chakra-ui/react';
-import Dropzone, { defaultClassNames, IDropzoneProps, ILayoutProps } from 'react-dropzone-uploader'
+import Dropzone, { defaultClassNames, IDropzoneProps, IFileWithMeta, ILayoutProps } from 'react-dropzone-uploader'
+import { create } from "ipfs-http-client";
 import 'react-dropzone-uploader/dist/styles.css';
+
+const client = create({ url: 'https://ipfs.infura.io:5001/api/v0'});
 
 // add type defs to custom LayoutComponent prop to easily inspect props passed to injected components
 const Layout = ({ input, previews, submitButton, dropzoneProps, files, extra: { maxFiles } }: ILayoutProps) => {
@@ -45,13 +49,44 @@ const InputContent = () => {
 
 
 const Uploader = () => {
+  const [file, setFile] = useState<Buffer | null>(null);
+  const [urlArr, setUrlArr] = useState<any[]>([]);
+
+  function readFileAsync(file: IFileWithMeta):Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      const reader = new window.FileReader();
+      reader.readAsArrayBuffer(file.file);
+  
+      reader.onloadend = () => {
+        console.log("Buffer data: ", new Buffer(reader.result as ArrayBuffer));
+        setFile(new Buffer(reader.result as ArrayBuffer));
+        resolve(new Buffer(reader.result as ArrayBuffer));
+      };
+  
+      reader.onerror = reject;
+    })
+  }
+
   // add type defs to function props to get TS support inside function bodies,
   // and not just where functions are passed as props into Dropzone
   const getUploadParams: IDropzoneProps['getUploadParams'] = () => ({ url: 'https://httpbin.org/post' })
 
-  const handleSubmit: IDropzoneProps['onSubmit'] = (files, allFiles) => {
-    console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
+  const handleSubmit: IDropzoneProps['onSubmit'] = async (files, allFiles) => {
+    console.log('files:', files);
+    console.log('allFiles:', allFiles);
+    for (let i = 0; i < allFiles.length; i++) {
+      const f = allFiles[i];
+      let arrayBuffer = await readFileAsync(f);
+      try {
+        const created = await client.add(arrayBuffer);
+        const url = `https://ipfs.infura.io/ipfs/${created.path}`;
+        console.log('url', url);
+        setUrlArr(prev => [...prev, url]);    
+      } catch (error) {
+        console.log(error);
+      }
+      f.remove() 
+    }
   }
   
   const { colorMode } = useColorMode()
